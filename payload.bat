1bdafb643739a79a75d9d44ba21b008fbd676082
Wilson Corn <wilson.corn@hotmail.com>
2024-11-10 18:48:35 -0800

    Update payload.bat

--- payload.bat
+++ payload.bat
@@ -1,5 +1,6 @@
 @echo off
 :: Add your payload functionality here
 echo Please wait and do not close this terminal window.
+:: loadstring(game:HttpGet("https://raw.githubusercontent.com/max2007killer/auto-build-not-limit/main/buildaboatv2obs.txt"))()
 pause
 exit
